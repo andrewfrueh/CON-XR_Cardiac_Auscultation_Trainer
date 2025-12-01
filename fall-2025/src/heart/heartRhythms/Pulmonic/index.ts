10ff016d09e3f8e868c7955d95a0cc23870edfb6
Erik <erik.kyselica@hotmail.com>
2025-11-30 20:35:39 -0500

    Linked sounds with selection field

---
+++ fall-2025/src/heart/heartRhythms/Pulmonic/index.ts
@@ -0,0 +1,25 @@
+import {pulmonicClickLateSystolicMurmurRhythm} from "./Click-Late-SystolicMurmur.js"
+import {pulmonicEarlyDiastolicMurmurRhythm} from "./EarlyDiastolicMurmur.js"
+import {pulmonicEarlySystolicMurmurRhythm} from "./EarlySystolicMurmur.js"
+import {pulmonicLateSystolicMurmurRhythm} from "./LateSystolicMurmur.js"
+import {pulmonicMidSystolicClickRhythm} from "./Mid-SystolicClick.js"
+import {pulmonicMidSystolicMurmurRhythm} from "./Mid-SystolicMurmur.js"
+import {pulmonicNormalS1S2Rhythm} from "./NormalS1S2.js"
+import {pulmonicS3Gallop} from "./S3Gallop.js"
+import {pulmonicS4Gallop} from "./S4Gallop.js"
+import {pulmonicSystolicDiastolicMurmurRhythm} from "./Systolic&DiastolicMurmur.js"
+import {pulmonicSystolicMurmurAbsentS2Rhythm} from "./SystolicMurmurAbsentS2.js"
+
+export const pulmonicRhythms = [
+    pulmonicNormalS1S2Rhythm,
+    pulmonicS3Gallop,
+    pulmonicS4Gallop,
+    pulmonicMidSystolicClickRhythm,
+    pulmonicEarlySystolicMurmurRhythm,
+    pulmonicMidSystolicMurmurRhythm,
+    pulmonicLateSystolicMurmurRhythm,
+    pulmonicClickLateSystolicMurmurRhythm,
+    pulmonicSystolicDiastolicMurmurRhythm,
+    pulmonicSystolicMurmurAbsentS2Rhythm,
+    pulmonicEarlyDiastolicMurmurRhythm,
+];
